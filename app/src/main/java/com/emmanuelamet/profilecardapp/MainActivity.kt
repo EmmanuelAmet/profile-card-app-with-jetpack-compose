0cbf030c78a015c0f840dc612d4ba37826ee7798
Emmanuel Ametepee <emmanuel.ametepee@turntabl.io>
2023-07-24 16:51:38 +0000

    ui updated

--- app/src/main/java/com/emmanuelamet/profilecardapp/MainActivity.kt
+++ app/src/main/java/com/emmanuelamet/profilecardapp/MainActivity.kt
@@ -1,43 +1,219 @@
 package com.emmanuelamet.profilecardapp
 
+import UserProfile
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
+import coil.transform.CircleCropTransformation
 import com.emmanuelamet.profilecardapp.ui.theme.ProfileCardAppTheme
+import com.emmanuelamet.profilecardapp.ui.theme.lightGreen
+import userProfileList
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ProfileCardAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                UsersApplication()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun UsersApplication(userProfiles: List<UserProfile> = userProfileList) {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "users_list") {
+        composable("users_list") {
+            UserListScreen(userProfiles, navController)
+        }
+        composable(
+            route = "user_details/{userId}",
+            arguments = listOf(navArgument("userId") {
+                type = NavType.IntType
+            })
+        ) { navBackStackEntry ->
+            UserProfileDetailsScreen(navBackStackEntry.arguments!!.getInt("userId"))
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun UserListScreen(userProfiles: List<UserProfile>, navController: NavHostController?) {
+    Scaffold(topBar = { AppBar() }) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            LazyColumn {
+                items(userProfiles) { userProfile ->
+                    ProfileCard(userProfile = userProfile) {
+                        navController?.navigate("user_details/${userProfile.id}")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun UserProfileDetailsScreen(userId: Int) {
+    val userProfile = userProfileList.first { userProfile -> userId == userProfile.id }
+    Scaffold(topBar = { AppBar() }) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+                ProfilePicture(userProfile.pictureUrl, userProfile.status, 240.dp)
+                ProfileContent(userProfile.name, userProfile.status, Alignment.CenterHorizontally)
+            }
+        }
+    }
+}
+
+@Composable
+fun AppBar() {
+    TopAppBar(
+        navigationIcon = {
+            Icon(
+                Icons.Default.Home,
+                contentDescription = "",
+                modifier = Modifier.padding(horizontal = 12.dp),
+            )
+        },
+        title = { Text("Profile Card App") }
+    )
+}
+
+@Composable
+fun ProfileCard(userProfile: UserProfile, clickAction: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(top = 8.dp, bottom = 4.dp, start = 16.dp, end = 16.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(align = Alignment.Top)
+            .clickable(onClick = { clickAction.invoke() }),
+        elevation = 8.dp,
+        backgroundColor = Color.White
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            ProfilePicture(userProfile.pictureUrl, userProfile.status, 72.dp)
+            ProfileContent(userProfile.name, userProfile.status, Alignment.Start)
+        }
+
+    }
+}
+
+@Composable
+fun ProfilePicture(pictureUrl: String, onlineStatus: Boolean, imageSize: Dp) {
+    Card(
+        shape = CircleShape,
+        border = BorderStroke(
+            width = 2.dp,
+            color = if (onlineStatus)
+                MaterialTheme.colors.lightGreen
+            else Color.Red
+        ),
+        modifier = Modifier
+            .padding(16.dp)
+            .size(imageSize),
+        elevation = 4.dp
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(
+                ImageRequest.Builder(LocalContext.current).data(data = pictureUrl).apply(block = fun ImageRequest.Builder.() {
+                    transformations(CircleCropTransformation())
+                }).build()
+            ),
+            modifier = Modifier.size(72.dp),
+            contentDescription = "Profile picture description",
+        )
+    }
+}
+
+@Composable
+fun ProfileContent(userName: String, onlineStatus: Boolean, alignment: Alignment.Horizontal) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp),
+        horizontalAlignment = alignment
+    ) {
+        CompositionLocalProvider(
+            LocalContentAlpha provides (
+                    if (onlineStatus)
+                        1f else ContentAlpha.medium)
+        ) {
+            Text(
+                text = userName,
+                style = MaterialTheme.typography.h5
+            )
+        }
+        CompositionLocalProvider(LocalContentAlpha provides (ContentAlpha.medium)) {
+            Text(
+                text = if (onlineStatus)
+                    "Active now"
+                else "Offline",
+                style = MaterialTheme.typography.body2
+            )
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UserProfileDetailsPreview() {
+    ProfileCardAppTheme {
+        UserProfileDetailsScreen(userId = 0)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ProfileCardAppTheme {
-        Greeting("Android")
+        UserListScreen(userProfiles = userProfileList, null)
     }
 }
--- app/src/main/java/com/emmanuelamet/profilecardapp/ui/theme/Type.kt
+++ app/src/main/java/com/emmanuelamet/profilecardapp/ui/theme/Type.kt
@@ -12,6 +12,12 @@ val Typography = Typography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        letterSpacing = 2.sp
     )
     /* Other default text styles to override
     button = TextStyle(
